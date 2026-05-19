f444f6406328ccccea62d8651d2b599c4d9442bc
User <gustavo.dev17@outlook.com>
2026-05-19 18:59:55 -0300

    create: project

---
+++ src/main/java/br/com/gustavo/literatus_api/LiteratusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.literatus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteratusApiApplication.class, args);
+	}
+
+}
