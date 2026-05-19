cef3865f04c8a5b70188b07493ed6a4f65b15ac6
User <gustavo.dev17@outlook.com>
2026-05-19 19:56:07 -0300

    create CategoriaResponse

---
+++ src/main/java/br/com/gustavo/literatus_api/dto/categoriaDto/CategoriaResponse.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.literatus_api.dto.categoriaDto;
+
+import br.com.gustavo.literatus_api.domain.Categoria;
+
+public record CategoriaResponse(
+
+        String nome
+) {
+        public CategoriaResponse(Categoria categoria) {
+            this(categoria.getNome());
+        }
+
+}
